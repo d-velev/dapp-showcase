e62f2cea8df8128e7bfcbfdc913705d3c4664494
d-velev <dimitar.velev@ingka.com>
2023-07-16 13:01:01 +0300

    Add vote function to contract

--- contracts/Showcase.sol
+++ contracts/Showcase.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.19;
 
 contract Showcase {
     uint private constant MINIMUM_DEPOSIT = 1 wei;
-    uint private constant BLOCKS_PER_VOTING_PERIOD = 1;
+    uint private constant BLOCKS_PER_VOTING_PERIOD = 2;
 
     struct Vote {
-        bytes32 commitmentHash;
+        bytes commitmentHash;
         uint amount;
     }
 
@@ -32,6 +32,7 @@ contract Showcase {
 
     string[] pendingDappAddresses;
     mapping(string => PendingDapp) public pendingDapps;
+
     mapping(string => bool) public approvedDapps;
 
     function getPendingDappAddresses() public view returns (string[] memory) {
@@ -57,5 +58,33 @@ contract Showcase {
         pendingDapp.submissionHeight = block.number;
         pendingDapp.rewardPool = 0;
         pendingDappAddresses.push(ipfsAddress);
+    }
+
+    function vote(
+        string memory ipfsAddress,
+        string memory commitmentHash
+    ) public payable {
+        PendingDapp storage pendingDapp = pendingDapps[ipfsAddress];
+
+        require(
+            pendingDapp.isInitialized &&
+                (block.number >= pendingDapp.submissionHeight &&
+                    block.number <=
+                    pendingDapp.submissionHeight + BLOCKS_PER_VOTING_PERIOD),
+            "Dapp not in voting period or pending"
+        );
+        require(
+            pendingDapp.voters[msg.sender].amount == 0,
+            "Address has already voted"
+        );
+        require(
+            bytes(commitmentHash).length == 66,
+            "Malformed commitment hash"
+        );
+        require(msg.value > 0, "Vote amount has to be positive");
+
+        pendingDapp.rewardPool += msg.value;
+        pendingDapp.voters[msg.sender].amount += msg.value;
+        pendingDapp.voters[msg.sender].commitmentHash = bytes(commitmentHash);
     }
 }
--- test/showcase.js
+++ test/showcase.js
@@ -1,26 +1,73 @@
 const Showcase = artifacts.require("Showcase");
 
 contract("Showcase", (accounts) => {
+    const ipfsAddress = "Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu"
+
     it("should add a new dapp hash only once", async () => {
-        const showcaseInstance = await Showcase.deployed();
-        await showcaseInstance.submitDapp("Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu", { from: accounts[0], value: 1 })
-        const dapp = await showcaseInstance.pendingDapps("Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu")
+        const showcaseInstance = await Showcase.new();
+        await showcaseInstance.submitDapp(ipfsAddress, { from: accounts[0], value: 1 })
+        const dapp = await showcaseInstance.pendingDapps(ipfsAddress)
 
         expect(dapp.isInitialized).to.be.true
 
         try {
-            await showcaseInstance.submitDapp("Qme7ss3ARVgxv6rXqVPiikMJ8u2NLgmgszg13pYrDKEoiu")
+            await showcaseInstance.submitDapp(ipfsAddress)
         } catch (error) {
             assert.equal(error.data.reason, "Dapp with this address already pending or approved")
         }
     });
     it("should fail if minimum deposit isn't given", async () => {
-        const showcaseInstance = await Showcase.deployed();
+        const showcaseInstance = await Showcase.new();
 
         try {
-            await showcaseInstance.submitDapp("QmNrgEMcUygbKzZeZgYFosdd27VE9KnWbyUD73bKZJ3bGi")
+            await showcaseInstance.submitDapp(ipfsAddress)
         } catch (error) {
             assert.equal(error.data.reason, "Transaction must include the minimum deposit")
+        }
+    });
+    it("should be able to vote for a dapp only once", async () => {
+        const showcaseInstance = await Showcase.new();
+
+        await showcaseInstance.submitDapp(ipfsAddress, { from: accounts[0], value: 1 })
+
+        const commitmentHash = "0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470";
+
+        await showcaseInstance.vote(ipfsAddress, commitmentHash, { from: accounts[0], value: 1 })
+
+        const dapp = await showcaseInstance.pendingDapps(ipfsAddress)
+
+        expect(dapp.rewardPool.toNumber()).to.equal(1)
+
+        try {
+            await showcaseInstance.vote(ipfsAddress, commitmentHash, { from: accounts[0], value: 1 })
+        } catch (error) {
+            assert.equal(error.data.reason, "Address has already voted")
+        }
+    });
+    it("should fail if commitment hash is malformed", async () => {
+        const showcaseInstance = await Showcase.new();
+
+        await showcaseInstance.submitDapp(ipfsAddress, { from: accounts[0], value: 1 })
+
+        const commitmentHash = "0xabc";
+
+        try {
+            await showcaseInstance.vote(ipfsAddress, commitmentHash, { from: accounts[0], value: 1 })
+        } catch (error) {
+            assert.equal(error.data.reason, "Malformed commitment hash")
+        }
+    });
+    it("should fail if no transaction value is provided", async () => {
+        const showcaseInstance = await Showcase.new();
+
+        await showcaseInstance.submitDapp(ipfsAddress, { from: accounts[0], value: 1 })
+
+        const commitmentHash = "0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470";
+
+        try {
+            await showcaseInstance.vote(ipfsAddress, commitmentHash, { from: accounts[0], value: 0 })
+        } catch (error) {
+            assert.equal(error.data.reason, "Vote amount has to be positive")
         }
     });
 });
